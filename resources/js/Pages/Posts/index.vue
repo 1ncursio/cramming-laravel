79c2b0f06a80e84fb5b3d698a5704bea24de4b28
1ncursio <ckswn1323@naver.com>
2021-10-26 20:45:50 +0900

    first commit

---
+++ resources/js/Pages/Posts/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <Layout>
+        <div v-for="(post, i) in posts.data" :key="post.id">
+            <post-card :post="post" />
+        </div>
+    </Layout>
+</template>
+<script>
+import Layout from "@/Layouts/AppLayout.vue";
+import PostCard from "./PostCard.vue";
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+    components: {
+        Layout,
+        PostCard,
+    },
+    props: ["posts"],
+    setup(props) {
+        console.log(props.posts);
+    },
+});
+</script>
+<style lang=""></style>
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
+
+Route::prefix('/posts')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+    Route::get('/create', [PostController::class, 'create'])->middleware(['auth']);
+
+    Route::get('/edit/{postId}', [PostController::class, 'edit'])->middleware(['auth'])->name('posts.edit');
+
+    Route::patch('/{postId}', [PostController::class, 'update'])->middleware(['auth'])->name('posts.update');
+
+    Route::post('/', [PostController::class, 'store'])->middleware(['auth'])->name('posts.store');
+
+    Route::delete('/{postId}', [PostController::class, 'destroy'])->middleware(['auth'])->name('posts.delete');
+
+    Route::get('/{postId}', [PostController::class, 'show'])->name('posts.show');
+});
