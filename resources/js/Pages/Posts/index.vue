dd977295124410874503f97104138d380de5baff
1ncursio <ckswn1323@naver.com>
2021-10-27 05:22:07 +0900

    gogo

--- resources/js/Pages/Posts/index.vue
+++ resources/js/Pages/Posts/index.vue
@@ -1,7 +1,9 @@
 <template>
     <app-layout title="Posts">
-        <Link :href="route('posts.create')">
-            <button
+        <pagination class="mt-6" :links="posts.links" />
+        <div class="flex justify-center mt-2">
+            <Link
+                :href="route('posts.create')"
                 class="
                     bg-indigo-500
                     hover:bg-indigo-600
@@ -10,20 +12,23 @@
                     py-2
                     px-4
                     rounded
+                    md:mx-auto
                 "
             >
                 Add new post
-            </button>
-        </Link>
+            </Link>
+        </div>
 
         <div v-for="(post, i) in posts.data" :key="i">
             <post-card :post="post" />
         </div>
+        <pagination class="mt-6" :links="posts.links" />
     </app-layout>
 </template>
 <script>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import PostCard from "./PostCard.vue";
+import Pagination from "./Pagination.vue";
 import { defineComponent } from "@vue/runtime-core";
 import { Link } from "@inertiajs/inertia-vue3";
 
@@ -32,6 +37,7 @@ export default defineComponent({
         AppLayout,
         PostCard,
         Link,
+        Pagination,
     },
     props: ["posts"],
     setup(props) {
